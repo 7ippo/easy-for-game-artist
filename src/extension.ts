292ba6f950d8eb26cb3bbe25e3e0e3bdfe174e7c
ZhouZibo <tingsven@163.com>
2019-04-12 21:19:36 +0800

    提供右键自定义菜单，功能未实现

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {FileSystemProvider} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,14 +14,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		const message = 'Hello VS Code!';
-		// Display a message box to the user
-		vscode.window.showInformationMessage(message);
+	let disposable = vscode.commands.registerCommand('extension.quickMakeDir', async (fileUri) => {
+		console.log(fileUri.fsPath);
+		
+	});
+
+	let disposable2 = vscode.commands.registerCommand('extension.quickMakePlayer', async (fileUri) => {
+		console.log(fileUri.fsPath);
+		
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
