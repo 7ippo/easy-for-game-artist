bea66e84cadfce6cb36eb0421825fa619edd7aeb
ZhouZibo <tingsven@163.com>
2019-04-13 16:38:30 +0800

    功能实现完全，简单测试没有问题

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,125 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {FileSystemProvider} from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
+type dirData = {
+	[index: string]: string[];
+};
+
+function makeDirRecursively(dir:string) {
+	if (fs.existsSync(dir)) { return; }
+	if (!fs.existsSync(path.dirname(dir))) {
+		makeDirRecursively(path.dirname(dir));
+	}
+	try {
+		fs.mkdirSync(dir);
+	} catch (error) {
+		vscode.window.showErrorMessage(error);	
+	}
+}
+
+function loopMakingDir(data:dirData, base_path:string){
+	for (const dir of Object.keys(data)) {
+		if (data[dir].length === 0) {
+			let path: string = base_path + '/' + dir;
+			console.log('Making Dir :'+path);
+			makeDirRecursively(path);
+			break;
+		}
+		for (const direction of data[dir]) {
+			let path: string = base_path + '/' + dir + '/' + direction;
+			console.log('Making Dir :'+path);
+			makeDirRecursively(path);
+		}
+	}
+}
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('Congratulations, your extension is now active!');
+
+	const playerDir: dirData = {
+		'stat': ['0', '45', '90', '135', '180'],
+		'behit': ['45', '135'],
+		'dead': ['45', '135'],
+		'run': ['0', '45', '90', '135', '180'],
+		'ride_stat': ['45', '135'],
+		'ride_run': ['45', '135'],
+		'attack': ['45', '135'],
+		'skill': ['45', '135'],
+		'jump': ['0', '45', '90', '135', '180'],
+		'hitdown': ['45', '135'],
+		'cbtskill': []
+	};
+
+	const npcDir: dirData = {
+		'stat': ['45', '135']
+	};
+
+	const monsterDir: dirData = {
+		'stat': ['45', '135'],
+		'behit': ['45', '135'],
+		'dead': ['45', '135'],
+		'run': ['45', '135'],
+		'attack': ['45', '135']
+	};
+
+	const mountDir: dirData = {
+		'stat': ['45', '135'],
+		'stat_up': ['45', '135'],
+		'run': ['45', '135'],
+		'run_up': ['45', '135']
+	};
+
+	const douhunDir: dirData = {
+		'stat': ['0', '45', '90', '135', '180'],
+		'run': ['0', '45', '90', '135', '180']
+	};
+
+	const shbbDir: dirData = {
+		'stat': ['0', '45', '90', '135', '180'],
+		'run': ['0', '45', '90', '135', '180'],
+		'attack': ['45', '135']
+	};
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.quickMakeDir', async (fileUri) => {
-		console.log(fileUri.fsPath);
-		
+	let disposable = vscode.commands.registerCommand('extension.quickMakePlayerDir', async (fileUri) => {
+		loopMakingDir(playerDir, fileUri.fsPath);
 	});
 
-	let disposable2 = vscode.commands.registerCommand('extension.quickMakePlayer', async (fileUri) => {
-		console.log(fileUri.fsPath);
-		
+	let disposable2 = vscode.commands.registerCommand('extension.quickMakeNpcDir', async (fileUri) => {
+		loopMakingDir(npcDir, fileUri.fsPath);
 	});
 
+	let disposable3 = vscode.commands.registerCommand('extension.quickMakeMonsterDir', async (fileUri) => {
+		loopMakingDir(monsterDir, fileUri.fsPath);
+	});
+
+	let disposable4 = vscode.commands.registerCommand('extension.quickMakeMountDir', async (fileUri) => {
+		loopMakingDir(mountDir, fileUri.fsPath);
+	});
+
+	let disposable5 = vscode.commands.registerCommand('extension.quickMakeDouhunDir', async (fileUri) => {
+		loopMakingDir(douhunDir, fileUri.fsPath);
+	});
+
+	let disposable6 = vscode.commands.registerCommand('extension.quickMakeShbbDir', async (fileUri) => {
+		loopMakingDir(shbbDir, fileUri.fsPath);
+	});
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposable2);
+	context.subscriptions.push(disposable3);
+	context.subscriptions.push(disposable4);
+	context.subscriptions.push(disposable5);
+	context.subscriptions.push(disposable6);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
