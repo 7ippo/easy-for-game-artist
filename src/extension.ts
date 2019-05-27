ae5ad90116707d14d6202c09984100468d0117bd
ZhouZibo <tingsven@163.com>
2019-05-27 12:46:26 +0800

    删除大量无用注释

--- src/extension.ts
+++ src/extension.ts
@@ -19,12 +19,12 @@ type framesJsonFile = {
 };
 
 type dirConstructFile = {
-	"playerDir"	: 	dirData,
-	"npcDir"	: 	dirData,
-	"monsterDir": 	dirData,
-	"mountDir"	: 	dirData,
-	"douhunDir"	: 	dirData,
-	"shbbDir"	: 	dirData,
+	"playerDir": dirData,
+	"npcDir": dirData,
+	"monsterDir": dirData,
+	"mountDir": dirData,
+	"douhunDir": dirData,
+	"shbbDir": dirData,
 
 };
 
@@ -36,7 +36,7 @@ function makeDirRecursively(dir: string) {
 	try {
 		fs.mkdirSync(dir);
 	} catch (error) {
-		vscode.window.showErrorMessage(error);	
+		vscode.window.showErrorMessage(error);
 	}
 }
 
@@ -44,20 +44,20 @@ function loopMakingDir(data: dirData, base_path: string) {
 	for (const dir of Object.keys(data)) {
 		if (data[dir].length === 0) {
 			let path: string = base_path + '/' + dir;
-			console.log('Making Dir :'+path);
+			console.log('Making Dir :' + path);
 			makeDirRecursively(path);
 			break;
 		}
 		for (const direction of data[dir]) {
 			let path: string = base_path + '/' + dir + '/' + direction;
-			console.log('Making Dir :'+path);
+			console.log('Making Dir :' + path);
 			makeDirRecursively(path);
 		}
 	}
 }
 
-function ReadFramesConfig(path: string): framesJsonFile{
-	let fileContent = fs.readFileSync(path, {encoding:"utf-8"});
+function ReadFramesConfig(path: string): framesJsonFile {
+	let fileContent = fs.readFileSync(path, { encoding: "utf-8" });
 	try {
 		return JSON.parse(fileContent);
 	} catch (error) {
@@ -66,8 +66,8 @@ function ReadFramesConfig(path: string): framesJsonFile{
 	}
 }
 
-function ReadDirConstructConfig(path: string): dirConstructFile{
-	let fileContent = fs.readFileSync(path, {encoding:"utf-8"});
+function ReadDirConstructConfig(path: string): dirConstructFile {
+	let fileContent = fs.readFileSync(path, { encoding: "utf-8" });
 	try {
 		return JSON.parse(fileContent);
 	} catch (error) {
@@ -85,10 +85,10 @@ function GetFramesArray(base_path: string): string[] | null {
 	if (fs.existsSync(base_path)) {
 		let files = fs.readdirSync(base_path);
 		files.forEach(element => {
-			let fullname = path.join(base_path,element);
+			let fullname = path.join(base_path, element);
 			let stats = fs.statSync(fullname);
 			// 把序列帧依次塞进队列
-			if (stats.isFile() && framesReg.test(element)){
+			if (stats.isFile() && framesReg.test(element)) {
 				frames.push(element);
 			}
 			if (configReg.test(element)) {
@@ -100,10 +100,10 @@ function GetFramesArray(base_path: string): string[] | null {
 			return null;
 		}
 		return frames;
-	}else{
+	} else {
 		return null;
 	}
-	
+
 }
 
 function ClassifyFrames(base_path: string, frames: string[], json_config: framesJsonFile) {
@@ -111,7 +111,7 @@ function ClassifyFrames(base_path: string, frames: string[], json_config: frames
 	json_config.struct.forEach(dir => {
 		let length = dir.length;
 		dir.directions.forEach(direction => {
-			for (let i = 0; i < length ; i++, frameCount++) {
+			for (let i = 0; i < length; i++ , frameCount++) {
 				let old_file = path.join(base_path, frames[frameCount]);
 				let new_file = path.join(base_path, dir.name, direction, frames[frameCount]);
 				fs.rename(old_file, new_file, (err) => {
@@ -132,7 +132,7 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('easymakingdir is now active!');
 
 	let disposable7 = vscode.commands.registerCommand('extension.quickClassifyFrames', async (fileUri) => {
-		let frames: string[]|null = GetFramesArray(fileUri.fsPath);
+		let frames: string[] | null = GetFramesArray(fileUri.fsPath);
 		if (!frames) {
 			return;
 		}
@@ -146,7 +146,6 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showWarningMessage('请检查config.json配置中序列帧的数量与文件夹下序列帧数量是否一致，当前配置会导致遍历越界');
 			return;
 		}
-
 		ClassifyFrames(fileUri.fsPath, frames, config);
 	});
 
@@ -156,7 +155,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Read Default Directory Construction from JSON
 	const dirConstructFilePath: string = "d:\\dirConstruct.json";
 
-	if (fs.existsSync(dirConstructFilePath)){
+	if (fs.existsSync(dirConstructFilePath)) {
 		let config: dirConstructFile = ReadDirConstructConfig(dirConstructFilePath);
 		const playerDir: dirData = config.playerDir;
 		const npcDir: dirData = config.npcDir;
@@ -168,85 +167,39 @@ export function activate(context: vscode.ExtensionContext) {
 		let disposable = vscode.commands.registerCommand('extension.quickMakePlayerDir', async (fileUri) => {
 			loopMakingDir(playerDir, fileUri.fsPath);
 		});
-	
+
 		let disposable2 = vscode.commands.registerCommand('extension.quickMakeNpcDir', async (fileUri) => {
 			loopMakingDir(npcDir, fileUri.fsPath);
 		});
-	
+
 		let disposable3 = vscode.commands.registerCommand('extension.quickMakeMonsterDir', async (fileUri) => {
 			loopMakingDir(monsterDir, fileUri.fsPath);
 		});
-	
+
 		let disposable4 = vscode.commands.registerCommand('extension.quickMakeMountDir', async (fileUri) => {
 			loopMakingDir(mountDir, fileUri.fsPath);
 		});
-	
+
 		let disposable5 = vscode.commands.registerCommand('extension.quickMakeDouhunDir', async (fileUri) => {
 			loopMakingDir(douhunDir, fileUri.fsPath);
 		});
-	
+
 		let disposable6 = vscode.commands.registerCommand('extension.quickMakeShbbDir', async (fileUri) => {
 			loopMakingDir(shbbDir, fileUri.fsPath);
 		});
-	
-		context.subscriptions.push(disposable, disposable2, 
+
+		context.subscriptions.push(disposable, disposable2,
 			disposable3, disposable4, disposable5, disposable6);
 		vscode.window.showInformationMessage('快速创建模板文件夹功能已启用');
-	}else{
+	} else {
 		vscode.window.showErrorMessage('找不到d:\\dirConstruct.json \n快速创建文件夹功能不会启用。请在该路径下添加json配置后重启VS Code以启用快速创建文件夹功能');
 		return;
 	}
 
-	// const playerDir: dirData = {
-	// 	'stat': ['0', '45', '90', '135', '180'],
-	// 	'behit': ['45', '135'],
-	// 	'dead': ['45', '135'],
-	// 	'run': ['0', '45', '90', '135', '180'],
-	// 	'ride_stat': ['45', '135'],
-	// 	'ride_run': ['45', '135'],
-	// 	'attack': ['45', '135'],
-	// 	'skill': ['45', '135'],
-	// 	'jump': ['0', '45', '90', '135', '180'],
-	// 	'hitdown': ['45', '135'],
-	// 	'htskill1': ['at/45', 'at/135', 'hit/45', 'hit/135'],
-	// 	'htskill2': ['at/45', 'at/135', 'hit/45', 'hit/135'],
-	// 	'htskill3': ['at/45', 'at/135', 'hit/45', 'hit/135']
-	// };
-
-	// const npcDir: dirData = {
-	// 	'stat': ['45', '135']
-	// };
-
-	// const monsterDir: dirData = {
-	// 	'stat': ['45', '135'],
-	// 	'behit': ['45', '135'],
-	// 	'dead': ['45', '135'],
-	// 	'run': ['0', '45', '90', '135', '180'],
-	// 	'attack': ['45', '135']
-	// };
-
-	// const mountDir: dirData = {
-	// 	'stat': ['45', '135'],
-	// 	'stat_up': ['45', '135'],
-	// 	'run': ['45', '135'],
-	// 	'run_up': ['45', '135']
-	// };
-
-	// const douhunDir: dirData = {
-	// 	'stat': ['0', '45', '90', '135', '180'],
-	// 	'run': ['0', '45', '90', '135', '180']
-	// };
-
-	// const shbbDir: dirData = {
-	// 	'stat': ['0', '45', '90', '135', '180'],
-	// 	'run': ['0', '45', '90', '135', '180'],
-	// 	'attack': ['45', '135']
-	// };
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	
+
 }
 
 // this method is called when your extension is deactivated
